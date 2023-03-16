0ae79b5404b4a1d8dae054768134493387a170f9
Jan Babák <babakhonza@gmail.com>
2023-03-16 11:40:47 +0100

    Initial Commit

---
+++ SpeedCubingTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeedCubingTimer
+//
+//  Created by Jan Babák on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SpeedCubingTimerUITests/SpeedCubingTimerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SpeedCubingTimerUITestsLaunchTests.swift
+//  SpeedCubingTimerUITests
+//
+//  Created by Jan Babák on 16.03.2023.
+//
+
+import XCTest
+
+final class SpeedCubingTimerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
