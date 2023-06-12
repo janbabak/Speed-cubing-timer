a237eb714440aa561f8d5597d2631d664e68035b
Jan Babák <babakhonza@gmail.com>
2023-06-12 21:54:35 +0200

    Add: live activity support
    - not work properly - live activity stops after 10s when the app is stopped after in the background

---
+++ SpeedCubingTimerActivityWidget/SpeedCubingTimerActivityWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SpeedCubingTimerActivityWidgetBundle.swift
+//  SpeedCubingTimerActivityWidget
+//
+//  Created by Jan Babák on 12.06.2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SpeedCubingTimerActivityWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SpeedCubingTimerActivityWidget()
+    }
+}
